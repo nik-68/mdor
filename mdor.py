1010c553ae7c51548ed6f0239c7be30c68e010ff
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-01 11:45:24 +0300

    Update mdor.py

--- mdor.py
+++ mdor.py
@@ -80,6 +80,7 @@ while True:
         print(pink + f"[LOG] PACKETS {error}")
         "print(cyan + f( DDoS)"
         print("CTRL+C to stop attack")
+    except KeyboardInterrupt:
 
     #Thread spawner
     for n in range(numberthreads):
